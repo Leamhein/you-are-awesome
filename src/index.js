d65a53e7d9873965a194deea35d7f7c45f786dd5
Vladimir <Leamhein@hotmail.com>
2018-03-01 17:55:29 +0300

    Done 6/10

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (n) => {return n};
+
+const createNotEnumerableProperty = (n) => {
+  Object.defineProperty(Object.prototype, n, {
+    enumerable: false, //hide property
+    get: () => Function.prototype.n, //getter
+    set: (n) => Function.prototype.n = n}); //setter
+    return n;
+  };
+
+
+const createProtoMagicObject = () => {
+  function magic () {}; // typeOf func != typeOf obj
+  magic.prototype = magic.__proto__; //make it equal
+return magic;
+};
+
+var counter = 0;
+const incrementor = () => {
+  incrementor.valueOf = () => {return counter}; //if need return number
+  counter++;
+  return incrementor; //return function
+};
+
+var count = 0;
+const asyncIncrementor = () => {
+  asyncIncrementor.valueOf = () => {return count}; //if need return number
+  count++;
+  return asyncIncrementor; //return function
+};
+
+const createIncrementer = () => {
+  return {}
+};
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = () => {};
 const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const createSerializedObject = () => {return null};
 const toBuffer = () => {};
 const sortByProto = () => {};
 
